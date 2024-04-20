12b7b988b3e9cdd1472a6581f1d0feac94453eab
wojci <wojciech.zysnarski91@gmail.com>
2024-04-20 16:02:43 +0200

    Create app layout

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 2136,
+    proxy: {
+      '/scrapper': {
+        target: 'http://localhost:2138',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/scrapper/, ''),
+      }
+    }
+  }
+})
