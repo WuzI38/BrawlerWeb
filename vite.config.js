8e3219b00a890224dd399a67a9ac06a8cc4e6264
wojci <wojciech.zysnarski91@gmail.com>
2024-04-24 19:50:32 +0200

    Add dockerfile

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,9 @@ export default defineConfig({
     port: 2136,
     proxy: {
       '/api': {
-        target: 'http://localhost:2138',
+        //target: 'http://localhost:2138',
+        //target: 'http://brawler_api:2138',
+        target: 'http://host.docker.internal:2138',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
